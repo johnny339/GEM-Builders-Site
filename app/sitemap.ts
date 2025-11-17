338a37981e589d7f31f0af672750474724829ca8
Johnny <guzmanj774@gmail.com>
2025-11-17 13:50:14 -0700

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,5 @@
 import { MetadataRoute } from 'next';
 
-module.exports = {
-  siteUrl: 'https://www.gembuildsco.com',
-  generateRobotsTxt: true,
-};
-
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://gembuildsco.com';
 
---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://www.gembuildsco.com',
+  generateRobotsTxt: true,
+};
